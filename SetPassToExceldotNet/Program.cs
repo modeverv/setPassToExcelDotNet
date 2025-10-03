e69739ae1582aba911972e14b91843f17f2e736a
seijiro <modeverv@gmail.com>
2025-10-03 15:18:22 +0900

    initial

---
+++ SetPassToExceldotNet/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DocumentFormat.OpenXml.Spreadsheet;
+
+Console.WriteLine("Hello, World!");
+
+var m = new SetPassToExceldotNet.Program();
+
