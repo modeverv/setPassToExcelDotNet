cd6358b5f6e9e3918cdb88490e5f9fe724752cf1
seijiro <modeverv@gmail.com>
2025-10-03 17:11:49 +0900

    add npoi sample

---
+++ npoitest/Program.cs
@@ -0,0 +1,23 @@
+﻿using NPOI.XSSF.UserModel;
+
+namespace npoitest;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // ワークブックを作成
+        var wb = new XSSFWorkbook();
+        wb.CreateSheet("s1");
+
+        var projectDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
+        var outputPath = Path.Combine(projectDir, "sample_b.xlsx");
+
+        using (var ms = new MemoryStream())
+        {
+            wb.Write(ms);
+            var bytes = ms.ToArray();
+            ExcelEncryptor.EncryptFromBytes(bytes, outputPath, "pass");
+        }
+    }
+}
